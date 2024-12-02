36d1d002736e413f489e521dfcd32b724b7d3ca8
Chess Seventh <chess7th@pm.me>
2024-12-02 08:43:10 +0100

    feat(go): add day 1

--- golang/cmd/root.go
+++ golang/cmd/root.go
@@ -21,23 +21,30 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//      Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute() string {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	var cmdFlag = rootCmd.Flag("day")
+	if cmdFlag == nil || len(cmdFlag.Value.String()) == 0 {
+		fmt.Println("No proper day has been set")
+		os.Exit(127)
+	}
+	return cmdFlag.Value.String()
 }
 
 func init() {
 	cobra.OnInitialize()
-
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
+	rootCmd.PersistentFlags().StringP("day", "d", "", "Day of the AoC 2024")
 }
---
+++ golang/days/day01.go
@@ -0,0 +1,87 @@
+package days
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Day01(fileContent string, column1 []int, column2 []int) {
+	var part01Result = Day01part01(fileContent, column1, column2)
+	var part02Result = Day01part02(fileContent, column1, column2)
+	fmt.Println(part01Result)
+	fmt.Println(part02Result)
+}
+
+func Day01part01(fileContent string, column1 []int, column2 []int) int {
+	var splitString = strings.Fields(fileContent)
+
+	column1, column2 = ExtractColumns(splitString, column1, column2)
+
+	column1 = SortColumn(column1)
+	column2 = SortColumn(column2)
+	return FindDistance(column1, column2)
+
+}
+
+func Day01part02(fileContent string, column1 []int, column2 []int) int {
+	var splitString = strings.Fields(fileContent)
+	column1, column2 = ExtractColumns(splitString, column1, column2)
+	column1 = SortColumn(column1)
+	column2 = SortColumn(column2)
+	numCount := make(map[int]int)
+
+	for i := 0; i < len(column1); i++ {
+		var counter = 0
+		for j := 0; j < len(column2); j++ {
+			if column1[i] == column2[j] {
+				counter++
+			}
+		}
+		numCount[column1[i]] = counter
+	}
+
+	var totalSum int = 0
+
+	//fmt.Println(numCount)
+
+	for key, value := range numCount {
+		totalSum += key * value
+	}
+	return totalSum
+}
+
+func SortColumn(column []int) []int {
+	//var sortedColumn []int
+	sort.Slice(column, func(i, j int) bool {
+		return column[i] < column[j]
+	})
+	return column
+}
+
+func FindDistance(column1 []int, column2 []int) int {
+	var totalSum int
+	for i := 0; i < len(column1); i++ {
+		var diffCol1Col2 int
+		if column1[i] > column2[i] {
+			diffCol1Col2 = column1[i] - column2[i]
+		} else {
+			diffCol1Col2 = column2[i] - column1[i]
+		}
+		totalSum += diffCol1Col2
+	}
+	return totalSum
+}
+
+func ExtractColumns(splitString []string, column1 []int, column2 []int) ([]int, []int) {
+	for i, word := range splitString {
+		intWord, _ := strconv.Atoi(word)
+		if i%2 == 0 {
+			column1 = append(column1, intWord)
+		} else {
+			column2 = append(column2, intWord)
+		}
+	}
+	return column1, column2
+}
