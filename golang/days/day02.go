ede267cf293c6c9e0ed86ba8439f6af2c694a18f
Chess Seventh <chess7th@pm.me>
2024-12-02 16:37:26 +0100

    feat(go): add day 2 - part 2

--- golang/days/day02.go
+++ golang/days/day02.go
@@ -2,89 +2,82 @@ package days
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
 
 func Day02(fileContent []string) {
 	part01(fileContent)
+	part02(fileContent)
 }
 
 func part01(fileContent []string) {
 	var safeLevels = 0
 	for line := range fileContent {
-		safeLevels += isLevelSafe(fileContent[line])
+		splitStringInt := convertLineToInt(fileContent[line])
+		safeLevels += isLevelSafe(splitStringInt)
 	}
 	fmt.Println("Part 01:", safeLevels)
 }
 
-func part02(fileContent []string) {
-	var safeLevels = 0
-	for line := range fileContent {
-		safeLevels += isLevelSafe(fileContent[line])
+func isLevelSafe(level []int) int {
+	valInc, _ := checkListIsIncreasing(level)
+	valDec, _ := checkListIsDecreasing(level)
+	if valInc || valDec {
+		return 1
 	}
+	return 0
 }
 
-func isLevelSafe(level string) int {
-	splitStringInt := convertLineToInt(level)
-	if !checkListOrdered(splitStringInt) {
-		return 0
-	}
-	return 1
-
-}
-
-func checkListOrdered(splitStringInt []int) bool {
-	var eitherIncOrDec bool
-	if checkListIsIncreasing(splitStringInt) {
-		return true
-	} else {
-		eitherIncOrDec = false
-	}
-
-	if checkListIsDecreasing(splitStringInt) {
-		return true
-	} else {
-		eitherIncOrDec = false
-	}
-	return eitherIncOrDec
-}
-
-func checkListIsIncreasing(level []int) bool {
+func checkListIsIncreasing(level []int) (bool, int) {
+	var mistakesCount = 0
+	var returnValue = true
 	for i := 0; i < len(level)-1; i++ {
-		if level[i] > level[i+1] || level[i] == level[i+1] {
-			return false
+		if level[i] > level[i+1] {
+			returnValue = false
+			mistakesCount++
+		}
+		if level[i] == level[i+1] {
+			returnValue = false
+			mistakesCount++
 		} else {
 			var distance = calculateDistance(level[i], level[i+1])
 			if !checkIfDistanceIsInBounds(distance) {
-				return false
+				mistakesCount++
+				returnValue = false
 			}
 		}
 	}
-	return true
+	return returnValue, mistakesCount
 }
 
-func checkListIsDecreasing(level []int) bool {
+func checkListIsDecreasing(level []int) (bool, int) {
+	var mistakesCount = 0
+	var returnValue = true
+
 	for i := 0; i < len(level)-1; i++ {
-		if level[i] < level[i+1] || level[i] == level[i+1] {
-			return false
+		if level[i] < level[i+1] {
+			returnValue = false
+			mistakesCount++
+		}
+		if level[i] == level[i+1] {
+			returnValue = false
+			mistakesCount++
 		} else {
 			var distance = calculateDistance(level[i], level[i+1])
 			if !checkIfDistanceIsInBounds(distance) {
-				return false
+				returnValue = false
+				mistakesCount++
 			}
 		}
 	}
-	return true
+	return returnValue, mistakesCount
 }
 
-func checkIfDistanceIsInBounds(tmpVal int) bool {
-	if tmpVal >= 1 {
-		if tmpVal <= 3 {
-			return true
-		}
-	} else {
-		return false
+func checkIfDistanceIsInBounds(value int) bool {
+	if value >= 1 && value <= 3 {
+		return true
 	}
 	return false
 }
@@ -108,4 +101,94 @@ func convertLineToInt(level string) []int {
 		splitStringInt = append(splitStringInt, j)
 	}
 	return splitStringInt
+}
+
+func part02(lines []string) {
+	var report [][]int
+	score := 0
+
+	for i := 0; i < len(lines); i++ {
+		currentLine := splitLines(lines[i], " ")
+		var currentLineAsInt []int
+		for j := 0; j < len(currentLine); j++ {
+			currentLineAsInt = append(currentLineAsInt, stringToNumber(currentLine[j]))
+		}
+		report = append(report, currentLineAsInt)
+	}
+
+	for i := 0; i < len(report); i++ {
+		goodResult := false
+		testReport := report[i]
+
+		for j := 0; j < len(testReport); j++ {
+			testSlice := make([]int, 0, len(testReport)-1)
+			testSlice = append(testSlice, testReport[:j]...)
+			testSlice = append(testSlice, testReport[j+1:]...)
+
+			if checkReport(testSlice) {
+				goodResult = true
+			}
+		}
+
+		if goodResult {
+			score += 1
+		}
+
+	}
+
+	println(score)
+
+}
+
+func checkReport(report []int) bool {
+	isValidResult := true
+
+	if report[0] < report[1] {
+		for j := 1; j < len(report); j++ {
+			if report[j] < report[j-1] {
+				isValidResult = false
+
+			}
+		}
+	} else if report[0] > report[1] {
+		for j := 1; j < len(report); j++ {
+			if report[j] > report[j-1] {
+				isValidResult = false
+
+			}
+		}
+	} else {
+		isValidResult = false
+
+	}
+
+	for j := 1; j < len(report); j++ {
+		if math.Abs(float64(report[j]-report[j-1])) > 0 &&
+			math.Abs(float64(report[j]-report[j-1])) < 4 {
+			// pass
+		} else {
+			isValidResult = false
+		}
+	}
+
+	if isValidResult == true {
+		return true
+	}
+	return false
+}
+
+func splitLines(line string, deliminator string) []string {
+	eachCharString := strings.Split(line, deliminator)
+	var cleanCharString []string
+	for i := 0; i < len(eachCharString); i++ {
+		if eachCharString[i] != "" {
+			cleanCharString = append(cleanCharString, eachCharString[i])
+		}
+	}
+	return cleanCharString
+}
+
+func stringToNumber(str string) int {
+	stringAsInt, _ := strconv.Atoi(str)
+	return stringAsInt
 }
